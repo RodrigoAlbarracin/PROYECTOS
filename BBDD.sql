790c50809d8a821b2df732b60839b65e84c8d426
RodrigoAlbarracin <rucky919@gmail.com>
2025-09-02 17:32:19 +0200

    Update BBDD.sql

---
+++ BBDD.sql
@@ -0,0 +1,15 @@
+SELECT flight_id, flight_number, status
+FROM flights
+WHERE status = 'On Time';
+
+SELECT * FROM bookings
+WHERE total_amount > 1000000;
+
+SELECT * FROM aircraft_data;
+
+SELECT flight_id
+FROM flights
+WHERE aircraft_code = '733';
+
+SELECT * FROM tickets
+WHERE passenger_name LIKE 'Irina%';
